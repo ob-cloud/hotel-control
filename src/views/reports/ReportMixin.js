35ccee77a55929a874e5be7d80efa4ce22ca0fa3
eamiear <justcall_me_k@163.com>
2020-12-04 18:31:36 +0800

    Fix:报表查询时间参数问题

--- src/views/reports/ReportMixin.js
+++ src/views/reports/ReportMixin.js
@@ -15,8 +15,7 @@ export const ReportMixin =  {
       this.searchQuery()
     },
     setParamDateString (bool) {
-      let startTime, endTime
-      if (bool && this.queryParam.type) {
+      const setType = () => {
         const type = this.queryParam.type
         const typeMap = {
           3: [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')],
@@ -26,10 +25,25 @@ export const ReportMixin =  {
         startTime = typeMap[type][0]
         endTime = typeMap[type][1]
         this.queryParam.date && (this.queryParam.date = null)
+      }
+      let startTime, endTime
+      if (bool && this.queryParam.type) {
+        // const type = this.queryParam.type
+        // const typeMap = {
+        //   3: [moment().format('YYYY-MM-DD'), moment().format('YYYY-MM-DD')],
+        //   2: [moment().startOf('month').format('YYYY-MM-DD'), moment().endOf('month').format('YYYY-MM-DD')],
+        //   1: [moment().startOf('year').format('YYYY-MM-DD'), moment().endOf('year').format('YYYY-MM-DD')]
+        // }
+        // startTime = typeMap[type][0]
+        // endTime = typeMap[type][1]
+        // this.queryParam.date && (this.queryParam.date = null)
+        setType()
       } else if (this.queryParam.date) {
         startTime = this.queryParam.date[0]
         endTime = this.queryParam.date[1]
         this.queryParam.type && (this.queryParam.type = undefined)
+      } else {
+        this.queryParam.type && setType()
       }
       this.queryParam.startTime = startTime
       this.queryParam.endTime = endTime
